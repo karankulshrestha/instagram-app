646f2d7a96d2b5b9bd89028849d0e1f66e57a1ea
Karan Kulshestha <karan2111001@akgec.ac.in>
2023-07-26 12:28:46 +0530

    added the login screen

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram/responses/mobile_screen_layout.dart';
+import 'package:instagram/responses/responsive_layout_screen.dart';
+import 'package:instagram/responses/web_screen_layout.dart';
+import 'package:instagram/screens/login_screen.dart';
+import 'package:instagram/utils/colors.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
